f29374e6750e1e3e2a1657c11b9eee164664f8a8
MATHEUS SOUSA DE FARIAS PAZ <25254220@educ123.sp.senai.br>
2026-08-07 09:59:38 -0300

    banco de dados das informacoes do curriculo

---
+++ create-table-template.sql
@@ -0,0 +1,43 @@
+create database curriculo_db;
+
+use curriculo_db;
+
+create table if not exists dados_pessoais(
+	id int auto_increment primary key,
+	nome VARCHAR(100) not null,
+	cargo VARCHAR(100),
+	resumo TEXT,
+	info_pessoais TEXT
+);
+
+create table  if not exists contatos(
+	id int auto_increment primary key,
+	dados_pessoais_id int not null,
+	email varchar(100) not null,
+	telefone varchar(20),
+	perfis_profissionais Text,
+	foreign key (dados_pessoais_id) references dados_pessoais(id) 
+	on delete cascade
+);
+
+create table if not exists experiencias(
+	id int auto_increment primary key,
+	dados_pessoais_id int not null,
+	empresa varchar(100) not null,
+	funcao varchar(100) not null,
+	periodo varchar(50),
+	descricao text,
+	foreign key (dados_pessoais_id) references dados_pessoais(id) 
+	on delete cascade 
+	);
+
+create table if not exists formacao(
+	id int auto_increment primary key,
+	dados_pessoais_id int not null,
+	instituicao varchar(100) not null,
+	curso varchar(100) not null,
+	periodo varchar(50),
+	foreign key (dados_pessoais_id) references dados_pessoais(id) 
+	on delete cascade 
+	
+);
