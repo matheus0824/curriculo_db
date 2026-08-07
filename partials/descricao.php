39af1fd0aa2d6affcef0747132085a3a0cd2ca68
MATHEUS SOUSA DE FARIAS PAZ <25254220@educ123.sp.senai.br>
2026-08-07 10:54:38 -0300

    logica para nome do usuario

--- partials/descricao.php
+++ partials/descricao.php
@@ -3,6 +3,4 @@ require_once 'partials/crud.php';
 
 $id_usuario = 1;
 
-$stmtPessoais = $pdo->prepare("select * from dados_pessoais where id = :id");
-$stmtPessoais->execute(['id => $id_usuario']);
-$dadosPessoais = $stmtPessoais->fetch(PDO::FETCH_ASSOC);
+
